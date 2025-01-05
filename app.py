f37284d8e41cb72c124d3eafde148b8642d3c2d0
Test126k <test126126k@gmail.com>
2025-01-05 20:37:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify({'status':'success','message':'hello'})
+    return jsonify({'status': 'success', 'message': 'hello'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)  # Use 0.0.0.0 to make it externally accessible
