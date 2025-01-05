60bdc460582580bd6924d1243a84dfb568c67b02
Test126k <test126126k@gmail.com>
2025-01-05 20:41:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,5 @@ def hello_world():
     return jsonify({'status': 'success', 'message': 'hello'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)  # Use 0.0.0.0 to make it externally accessible
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
