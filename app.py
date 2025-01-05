e517db9c0710e0a8079a3d546d038791e0e1fa92
Test126k <test126126k@gmail.com>
2025-01-05 20:44:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello_world():
     return jsonify({'status': 'success', 'message': 'hello'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8080))  # Use Koyeb's PORT environment variable
+    app.run(host='0.0.0.0', port=port)  # Bind to all network interfaces
